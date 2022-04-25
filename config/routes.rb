6191e80c1416452bad30af7bda77946bd75c3173
Michael Duvenary <m.duvenary@gmail.com>
2022-04-24 23:47:33 -0700

    Added login functionality along with button styles.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   
   get 'sign_up', to: 'registration#new'
   post 'sign_up', to: 'registration#create'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+
+
   delete 'logout', to: 'sessions#destroy'
   root to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
